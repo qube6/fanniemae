c9148300a29733ee34ae2e51446451bce325084a
Yvonne Takane <ytakane@navigationarts.com>
2016-03-08 12:21:35 -0500

    Replacing HTML5 error validation with angular validation for cross-browser compatibility

--- client/src/components/contact-form/fm-contact-form.js
+++ client/src/components/contact-form/fm-contact-form.js
@@ -10,6 +10,7 @@ directiveModule.directive('fmContactForm', [
       $scope.contact.contactText = '';
       $scope.contact.contactEmail = null;
       $scope.contact.showForm = false;
+      $scope.contact.showErrors = false;
 
       $scope.submitParent = function(){
         fannieAPIservice.getData($scope.contact.apiUrl, $scope.contact.topicBox)
@@ -26,10 +27,20 @@ directiveModule.directive('fmContactForm', [
             $scope.contact.text = $sce.trustAsHtml(result.text);
         });
       }
+
+      $scope.validate = function(form){
+        if (form.$valid) {
+           var e = document.getElementsByName(form.$name);
+           e[0].submit();
+        } else{
+          $scope.contact.showErrors = true;
+        }
+      }
     }
 
+
     return {
-      restrict: 'A',
+      restrict: 'E',
       link: link,
       scope: true
     };
