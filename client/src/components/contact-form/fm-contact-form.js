ed192efc4c052b00c23ef666c2335eb491ef353b
Yvonne Takane <ytakane@navigationarts.com>
2016-03-08 11:20:42 -0500

    Contact Us is a dynamic page. The Sub-Topic drop-down, contact blurb, phone number, and form are NOT displayed by default. Once the user selects a Topic, the Sub-topic drop-down menu is displayed. Once the user selects a Sub-Topic, the contact blurb, phone number and form are displayed.

--- client/src/components/contact-form/fm-contact-form.js
+++ client/src/components/contact-form/fm-contact-form.js
@@ -1,19 +1,29 @@
 directiveModule.directive('fmContactForm', [
   'fannieAPIservice',
   '$timeout',
-  function (fannieAPIservice, $timeout) {
+  '$sce',
+  function (fannieAPIservice, $timeout, $sce) {
     var link = function($scope, $element, attr){
       $scope.contact = {};
       $scope.contact.apiUrl = attr.apiUrl;
-      $scope.contact.selectChild = null;
-      $scope.contact.selectOptions = [{ id: 'Select', name: 'Select'}];
+      $scope.contact.selectOptions = [];
+      $scope.contact.contactText = '';
+      $scope.contact.contactEmail = null;
+      $scope.contact.showForm = false;
 
       $scope.submitParent = function(){
-        fannieAPIservice.getData($scope.contact.apiUrl, $scope.contact.selectParent)
+        fannieAPIservice.getData($scope.contact.apiUrl, $scope.contact.topicBox)
         .success(function (result) {
             $scope.contact.selectOptions = result.options;
-            $scope.contact.selectOptions.unshift({ id: '0', name: 'Select'});
-            $scope.contact.selectChild = $scope.contact.selectOptions[0];
+        });
+      }
+
+      $scope.submitChild = function(){
+        fannieAPIservice.getData($scope.contact.apiUrl, $scope.contact.topicBox)
+        .success(function (result) {
+            $scope.contact.showForm = true;
+            $scope.contact.email = result.email;
+            $scope.contact.text = $sce.trustAsHtml(result.text);
         });
       }
     }
