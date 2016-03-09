b799a58dd5d8e3134a2d81d94545884eb8738923
avespoli <avespoli@navigationarts.com>
2016-03-09 12:33:48 -0700

    added content to PT-03 and PT06

--- client/build/js/main.js
+++ client/build/js/main.js
@@ -409,6 +409,52 @@ directiveModule.directive('fmContactForm', [
     };
   }
 ]);
+directiveModule.directive('fmDropdown', [
+  function () {
+    var link = function ($scope, element, attrs, controller) {
+      $scope.select = element.find('select');
+      if ($scope.select.length != 1){
+        console.err('fm-dropdown must contain 1 select element only');
+        return;
+      }
+
+      $scope.selectlist = createSelectList( $scope.select );
+      $scope.selectedOption = getSelected($scope.select[0]);
+
+      $scope.setSelected = function(val){
+        $scope.select.val(val);
+        $scope.selectedOption = getSelected($scope.select[0]);
+        $scope.select[0].onchange();
+      }
+
+    };
+    var getSelected = function(select){
+      if (select.selectedOptions && select.selectedOptions.length == 1){
+        return select.selectedOptions[0];
+      }
+    }
+    var createSelectList = function(select){
+      var optList = select.find('option'),
+          list = []
+      angular.forEach(optList, function(option){
+        if (!option.disabled){
+          list.push({
+            text: option.text,
+            value: option.value
+          })
+        }
+      });
+      return list;
+    }
+    
+    return {
+      restrict: 'E',
+      link: link,
+      scope: true,
+      transclude: true,
+      template: "<div aria-hidden='true' class='fm-select-wrapper' fm-accordion><div class='fm-styled-select' fm-accordion-item ng-class='{ \"open\" : isOpen() }' ng-click='toggleItem()'>{{selectedOption.text}}</span></div><ul fm-accordion-item class='fm-select-submenu' ng-click='toggleItem()'><li ng-repeat='option in selectlist' ng-click='setSelected(option.value)'>{{option.text}}</li></ul></div><div ng-transclude class='sr-only'></div>"
+    };
+}])
 directiveModule.directive('fmVideoPlayer', ['$timeout',
   function($timeout) {
     var link = function ($scope, element, attrs) {
@@ -455,52 +501,6 @@ directiveModule.directive('fmVideoPlayer', ['$timeout',
       scope: true,
       transclude: true,
       template: '<div class="fm-video-wrapper"><span class="icon" ng-class=" videoPaused ? \'fm-play-circle\' : \'fm-pause-circle\' " ng-click="controlVideo()"></span><video ng-transclude></video></div>'
-    };
-}])
-directiveModule.directive('fmDropdown', [
-  function () {
-    var link = function ($scope, element, attrs, controller) {
-      $scope.select = element.find('select');
-      if ($scope.select.length != 1){
-        console.err('fm-dropdown must contain 1 select element only');
-        return;
-      }
-
-      $scope.selectlist = createSelectList( $scope.select );
-      $scope.selectedOption = getSelected($scope.select[0]);
-
-      $scope.setSelected = function(val){
-        $scope.select.val(val);
-        $scope.selectedOption = getSelected($scope.select[0]);
-        $scope.select[0].onchange();
-      }
-
-    };
-    var getSelected = function(select){
-      if (select.selectedOptions && select.selectedOptions.length == 1){
-        return select.selectedOptions[0];
-      }
-    }
-    var createSelectList = function(select){
-      var optList = select.find('option'),
-          list = []
-      angular.forEach(optList, function(option){
-        if (!option.disabled){
-          list.push({
-            text: option.text,
-            value: option.value
-          })
-        }
-      });
-      return list;
-    }
-    
-    return {
-      restrict: 'E',
-      link: link,
-      scope: true,
-      transclude: true,
-      template: "<div aria-hidden='true' class='fm-select-wrapper' fm-accordion><div class='fm-styled-select' fm-accordion-item ng-class='{ \"open\" : isOpen() }' ng-click='toggleItem()'>{{selectedOption.text}}</span></div><ul fm-accordion-item class='fm-select-submenu' ng-click='toggleItem()'><li ng-repeat='option in selectlist' ng-click='setSelected(option.value)'>{{option.text}}</li></ul></div><div ng-transclude class='sr-only'></div>"
     };
 }])
 directiveModule.directive('fmListing', 
