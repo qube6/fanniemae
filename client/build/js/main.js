e1679eaf6db82d251c2d165a65ed2cc5ce1bfe3e
Yvonne Takane <ytakane@navigationarts.com>
2016-03-14 17:13:55 -0400

    Updating logo image  - requires updates to Header/Footer (view box on svgs)

--- client/build/js/main.js
+++ client/build/js/main.js
@@ -336,14 +336,13 @@ directiveModule.directive('fmAccordion', [
   '$timeout', 
   function ($timeout) {
     var link = function ($scope, element, attrs, controller) {
-        this.closeOthers = attrs.closeOthers == undefined ? true : eval(attrs.closeOthers);
-        this.clickAway = attrs.clickAway == undefined ? false : eval(attrs.clickAway);
+        $scope.closeOthers = attrs.closeOthers == undefined ? true : eval(attrs.closeOthers);
+        $scope.clickAway = attrs.clickAway == undefined ? false : eval(attrs.clickAway);
     };
     
     return {
       restrict: 'EA',
       link: link,
-      // scope: true
       bindToController: true,
       scope: true,
       controllerAs: "$ctrl",
@@ -357,6 +356,7 @@ directiveModule.directive('fmAccordion', [
             $scope.$broadcast('fmAccordionStateChange', $scope.allOpen);
           }, 0);
         }
+
       }
     };
 }])
@@ -367,7 +367,6 @@ directiveModule.directive('fmAccordion', [
     var link = function ($scope, element, attrs, controller) {
       $scope.open = attrs.open != undefined;
       $scope.ignoreBroadcast = false;
-      var closeOthers = controller.closeOthers;
 
       $scope.toggleItem = function(){
         $timeout(function(){
@@ -375,13 +374,13 @@ directiveModule.directive('fmAccordion', [
           if($scope.open){
             $scope.ignoreBroadcast = true;
             $scope.$parent.$broadcast('fmAccordionItemOpen');
-            if(closeOthers) setPageScroll(element[0]);
+            if($scope.$parent.closeOthers) setPageScroll(element[0]);
           }
         }, 0);
       };
 
       $scope.$on('fmAccordionItemOpen', function(){
-        if(closeOthers && !$scope.ignoreBroadcast){
+        if($scope.$parent.closeOthers && !$scope.ignoreBroadcast){
           $scope.open = false;
         }
         $scope.ignoreBroadcast = false;
