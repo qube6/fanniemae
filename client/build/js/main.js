488dde4df02defd55933373960d948f87fca8b68
avespoli <avespoli@navigationarts.com>
2016-03-18 13:41:19 -0700

    last changes for fannie

--- client/build/js/main.js
+++ client/build/js/main.js
@@ -332,6 +332,77 @@ angular.module('fannieMae').factory('fannieAPIservice', function($http) {
   
   return fannieAPI;
 });
+directiveModule.directive('fmAccordion', [
+  '$timeout', 
+  function ($timeout) {
+    var link = function ($scope, element, attrs, controller) {
+        $scope.closeOthers = attrs.closeOthers == undefined ? true : eval(attrs.closeOthers);
+        $scope.clickAway = attrs.clickAway == undefined ? false : eval(attrs.clickAway);
+    };
+    
+    return {
+      restrict: 'EA',
+      link: link,
+      bindToController: true,
+      scope: true,
+      controllerAs: "$ctrl",
+      controller: function ($scope) {
+
+        $scope.allOpen = false;
+
+        $scope.setAll = function(){
+          $timeout(function(){
+            $scope.allOpen = !$scope.allOpen;
+            $scope.$broadcast('fmAccordionStateChange', $scope.allOpen);
+          }, 0);
+        }
+
+      }
+    };
+}])
+
+.directive('fmAccordionItem', [
+  '$timeout',
+  function ($timeout) {
+    var link = function ($scope, element, attrs, controller) {
+      $scope.open = attrs.open != undefined;
+      $scope.ignoreBroadcast = false;
+
+      $scope.toggleItem = function(){
+        $timeout(function(){
+          $scope.open = !$scope.open;
+          if($scope.open){
+            $scope.ignoreBroadcast = true;
+            $scope.$parent.$broadcast('fmAccordionItemOpen');
+            if($scope.$parent.closeOthers) setPageScroll(element[0]);
+          }
+        }, 0);
+      };
+
+      $scope.$on('fmAccordionItemOpen', function(){
+        if($scope.$parent.closeOthers && !$scope.ignoreBroadcast){
+          $scope.open = false;
+        }
+        $scope.ignoreBroadcast = false;
+      });
+
+      $scope.$on('fmAccordionStateChange', function($e, state){
+        $scope.open = state;
+      });
+
+      $scope.isOpen = function(){
+        return $scope.open;
+      };
+
+    };
+    
+    return {
+      require: '^fmAccordion',
+      restrict: 'EA',
+      link: link,
+      scope: true
+    };
+}]);
 directiveModule.directive('fmContactForm', [
   'fannieAPIservice',
   '$timeout',
@@ -466,77 +537,6 @@ directiveModule.directive('fmDropdown', [
       template: "<div aria-hidden='true' class='fm-select-wrapper' fm-accordion><div class='fm-styled-select' fm-accordion-item ng-class='{ \"open\" : isOpen() }' ng-click='toggleItem()'>{{selectedOption.text}}</span></div><ul fm-accordion-item class='fm-select-submenu' ng-click='toggleItem()'><li ng-repeat='option in selectlist' ng-click='setSelected(option.value)'>{{option.text}}</li></ul></div><div ng-transclude class='sr-only'></div>"
     };
 }])
-directiveModule.directive('fmAccordion', [
-  '$timeout', 
-  function ($timeout) {
-    var link = function ($scope, element, attrs, controller) {
-        $scope.closeOthers = attrs.closeOthers == undefined ? true : eval(attrs.closeOthers);
-        $scope.clickAway = attrs.clickAway == undefined ? false : eval(attrs.clickAway);
-    };
-    
-    return {
-      restrict: 'EA',
-      link: link,
-      bindToController: true,
-      scope: true,
-      controllerAs: "$ctrl",
-      controller: function ($scope) {
-
-        $scope.allOpen = false;
-
-        $scope.setAll = function(){
-          $timeout(function(){
-            $scope.allOpen = !$scope.allOpen;
-            $scope.$broadcast('fmAccordionStateChange', $scope.allOpen);
-          }, 0);
-        }
-
-      }
-    };
-}])
-
-.directive('fmAccordionItem', [
-  '$timeout',
-  function ($timeout) {
-    var link = function ($scope, element, attrs, controller) {
-      $scope.open = attrs.open != undefined;
-      $scope.ignoreBroadcast = false;
-
-      $scope.toggleItem = function(){
-        $timeout(function(){
-          $scope.open = !$scope.open;
-          if($scope.open){
-            $scope.ignoreBroadcast = true;
-            $scope.$parent.$broadcast('fmAccordionItemOpen');
-            if($scope.$parent.closeOthers) setPageScroll(element[0]);
-          }
-        }, 0);
-      };
-
-      $scope.$on('fmAccordionItemOpen', function(){
-        if($scope.$parent.closeOthers && !$scope.ignoreBroadcast){
-          $scope.open = false;
-        }
-        $scope.ignoreBroadcast = false;
-      });
-
-      $scope.$on('fmAccordionStateChange', function($e, state){
-        $scope.open = state;
-      });
-
-      $scope.isOpen = function(){
-        return $scope.open;
-      };
-
-    };
-    
-    return {
-      require: '^fmAccordion',
-      restrict: 'EA',
-      link: link,
-      scope: true
-    };
-}]);
 directiveModule.directive('fmListing', 
   function(fannieAPIservice) {
     var link = function ($scope, $element, attrs) {
